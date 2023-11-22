847549b9c0016e73d0de82d155dc2ca984a67d63
binhntph28014 <binhntph28014@fpt.edu.vn>
2023-11-22 16:59:16 +0700

    Initial commit

---
+++ app/src/main/java/binhntph28014/fpoly/nhom11_ql_bbt_coffee/MainActivity.java
@@ -0,0 +1,14 @@
+package binhntph28014.fpoly.nhom11_ql_bbt_coffee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
