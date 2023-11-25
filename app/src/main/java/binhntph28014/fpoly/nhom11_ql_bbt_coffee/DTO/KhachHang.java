3de08e9cafa9d5302ad8f79d5f9f7c46b66708d4
binhntph28014 <binhntph28014@fpt.edu.vn>
2023-11-25 23:24:57 +0700

    DTO_KhachHang

---
+++ app/src/main/java/binhntph28014/fpoly/nhom11_ql_bbt_coffee/DTO/KhachHang.java
@@ -0,0 +1,52 @@
+package binhntph28014.fpoly.nhom11_ql_bbt_coffee.DTO;
+
+public class KhachHang {
+    private int maKH;
+    private String hoTen;
+    private String sdt;
+    private String namSinh;
+    private int gioiTinh;
+
+    public KhachHang() {
+    }
+
+    public int getMaKH() {
+        return maKH;
+    }
+
+    public void setMaKH(int maKH) {
+        this.maKH = maKH;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public String getNamSinh() {
+        return namSinh;
+    }
+
+    public void setNamSinh(String namSinh) {
+        this.namSinh = namSinh;
+    }
+
+    public int getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(int gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+}
